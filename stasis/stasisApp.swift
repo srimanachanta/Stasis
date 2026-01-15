ceddc035a37e73c7d2bc82c3c5fa9ac4e5a6a1f8
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2026-01-15 18:10:05 -0500

    Initial Commit

---
+++ stasis/stasisApp.swift
@@ -0,0 +1,17 @@
+//
+//  stasisApp.swift
+//  stasis
+//
+//  Created by Sriman Achanta on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct stasisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
